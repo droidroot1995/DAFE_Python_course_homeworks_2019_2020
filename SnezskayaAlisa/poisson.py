fe8ffe3e23b2d81f50d8a5566664ad2b8e598c26
alisasnezskaya <48555987+alisasnezskaya@users.noreply.github.com>
2020-05-10 16:34:21 +0300

    Update poisson.py

--- SnezskayaAlisa/poisson.py
+++ SnezskayaAlisa/poisson.py
@@ -2,9 +2,16 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import math
 x = np.random.poisson(1.5, 1000)
 n,bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
-y = ((np.exp(-1.5))*1.5*x)/(np.math.factorial(x))
+
+def myfunction(x):
+  return np.math.factorial(x)
+
+myfunction2 = np.vectorize(myfunction)
+
+y = ((np.exp(-1.5))*1.5*x)/(myfunction2(x))
 plt.plot(y, '--')  
 plt.title('Poisson')
 plt.text(60, .025, r'$\lambda=1.5')

136c9a8a9da523d3fd00362aad077aabab510a5f
lebedevarina <48378479+lebedevarina@users.noreply.github.com>
2020-05-10 15:18:57 +0300

    Update diff.py

--- Arina_Lebedeva/numerical_methods/diff.py
+++ Arina_Lebedeva/numerical_methods/diff.py
@@ -13,10 +13,11 @@ def d_fun(x):
 x0 = float(input())
 x1 = float(input())
 x = random.uniform(x0, x1)
+print (x)
 print (d_fun(x))
 
 x = np.arange(-10, 10, 0.2)
-y = d_fun(x)
+y = fun(x)
 fig, axs = plt.subplots()
 plt.axis([-10, 10, - 10, 10])
 plt.scatter(x, y)
